8f302883c207017d28f0be87d9559d675fed3cde
Laura Badgley <l.badgleyy@gmail.com>
2019-06-11 14:48:10 -0700

    added routing to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import CreateNote from '../container/notes/CreateNoteContainer';
-import AllNotes from '../container/notes/AllNotes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import Callback from '../container/auth/Callback';
+import { withSession } from '../container/auth/withSession';
+// import CreateNote from '../container/notes/CreateNoteContainer';
+// import AllNotes from '../container/notes/AllNotes';
 
 export default function App() {
   return (
-    <>
-      <CreateNote />
-      <AllNotes />
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={withSession(Home)} />
+        <Route path='/callback' component={Callback }/>
+      </Switch>
+    </Router>
   );
 }
