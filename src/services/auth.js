04a15d1c05b534ee4787ae782f8b2c0955208e4f
Laura Badgley <l.badgleyy@gmail.com>
2019-06-11 13:42:57 -0700

    setting up auth.js

---
+++ src/services/auth.js
@@ -0,0 +1,14 @@
+// import { WebAuth } from 'auth0-js';
+
+// const auth0 = new WebAuth({
+//   domain: 
+//   clientId:
+//   redirectUri:
+//   responseType:
+//   scope:
+
+// });
+
+// export default function App() {
+//   login();
+// }
