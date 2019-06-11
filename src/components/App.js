d27c77827d42066bd388643738d052802739a45d
Laura Badgley <l.badgleyy@gmail.com>
2019-06-11 14:09:02 -0700

    building client stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import CreateNote from '../container/notes/CreateNoteContainer';
-// import AllNotes from '../container/notes/AllNotes';
+import AllNotes from '../container/notes/AllNotes';
 
 export default function App() {
   return (
     <>
       <CreateNote />
-      {/* <AllNotes /> */}
+      <AllNotes />
     </>
   );
 }
